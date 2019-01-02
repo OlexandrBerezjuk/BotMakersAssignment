4132ca4c030d609aa230d8e6a4b5090280091362
Alexandr1986Git <berezjukalexandr@gmail.com>
2019-01-02 22:25:15 +0200

    Updates

---
+++ precache-manifest.ae0e64af9afa498abc5f1c8db964d70e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82d490ca0ce3d127ef7a",
+    "url": "/olexandrberezjuk.github.io/BotMakersAssignment/static/js/runtime~main.82d490ca.js"
+  },
+  {
+    "revision": "a6a84be4e8719726969f",
+    "url": "/olexandrberezjuk.github.io/BotMakersAssignment/static/js/main.a6a84be4.chunk.js"
+  },
+  {
+    "revision": "2c1af290294ac889f0da",
+    "url": "/olexandrberezjuk.github.io/BotMakersAssignment/static/js/1.2c1af290.chunk.js"
+  },
+  {
+    "revision": "a6a84be4e8719726969f",
+    "url": "/olexandrberezjuk.github.io/BotMakersAssignment/static/css/main.7368ba6a.chunk.css"
+  },
+  {
+    "revision": "8271df7fba6fca22ccc4658476ca70df",
+    "url": "/olexandrberezjuk.github.io/BotMakersAssignment/index.html"
+  }
+];

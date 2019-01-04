fa71447e4c8cc451a3cabe427c42b5f86c9f8839
Alexandr1986Git <berezjukalexandr@gmail.com>
2019-01-04 10:07:45 +0200

    Updates

---
+++ precache-manifest.ff67e9388ec320afbbab87467c026d39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ff27a31c0aeda5ea74a6",
+    "url": "./static/js/main.ff27a31c.chunk.js"
+  },
+  {
+    "revision": "2c1af290294ac889f0da",
+    "url": "./static/js/1.2c1af290.chunk.js"
+  },
+  {
+    "revision": "ff27a31c0aeda5ea74a6",
+    "url": "./static/css/main.7368ba6a.chunk.css"
+  },
+  {
+    "revision": "cda26406a9106e58d650c4997c2afe55",
+    "url": "./index.html"
+  }
+];
